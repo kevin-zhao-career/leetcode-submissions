7540de62e2509208742e3ed48c60bab45a7ae54e
Kevin Zhao <kevin.zhao.career@outlook.com>
2021-02-05 11:33:20 -0800

    [2021-02-05] Added cpp solution to problem 34.

---
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -0,0 +1,26 @@
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& numbers, int target) {
+        if (numbers.empty()) {
+            return {-1, -1};
+        }
+        
+        auto iterator_range = equal_range(numbers.begin(),
+                                          numbers.end(),
+                                          target);
+        auto begin_index = static_cast<int>(iterator_range.first - numbers.begin());
+        auto end_index = static_cast<int>(iterator_range.second - numbers.begin()) - 1;
+        
+        begin_index = ((begin_index != numbers.size()) && (numbers[begin_index] == target)) ?
+            (begin_index) : (-1);
+        end_index = ((end_index != -1) && (numbers[end_index ] == target)) ?
+            (end_index) : (-1);
+
+        return {begin_index, end_index};
+    }
+};
