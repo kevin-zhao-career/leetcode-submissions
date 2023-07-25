27018ca6db57026ec38c8b12186bb321ea15ac5e
kevin-zhao-career <71047577+kevin-zhao-career@users.noreply.github.com>
2023-07-25 17:11:18 -0400

    Create 2770-maximum-number-of-jumps-to-reach-the-last-index.cpp

---
+++ 2770-maximum-number-of-jumps-to-reach-the-last-index/2770-maximum-number-of-jumps-to-reach-the-last-index/2770-maximum-number-of-jumps-to-reach-the-last-index.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int maximumJumps(vector<int> & numbers, int target) {
+        auto number_size = numbers.size();
+        
+        vector<int> number_queue(number_size, -1);
+        number_queue[0] = 0;
+        for (auto source_index = 0; source_index != number_size; ++source_index) {
+            if(number_queue[source_index] == -1) {
+                continue;
+            }
+            for(auto destination_index = source_index + 1; destination_index != number_size; ++destination_index) {
+                if(abs(numbers[destination_index] - numbers[source_index]) <= target) {
+                    number_queue[destination_index] = max(number_queue[source_index] + 1, number_queue[destination_index]);
+                }  
+            }
+        }
+        return number_queue[number_size - 1];
+    }
+};
