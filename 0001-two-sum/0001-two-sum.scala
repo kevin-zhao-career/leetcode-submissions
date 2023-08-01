82e82e0ce501b20721c3eea85f20f96bb5ea4247
kevin-zhao-career <71047577+kevin-zhao-career@users.noreply.github.com>
2023-08-01 13:21:55 -0400

    Update 0001-two-sum.scala

--- 0001-two-sum/0001-two-sum.scala
+++ 0001-two-sum/0001-two-sum.scala
@@ -1,16 +1,11 @@
 object Solution {
-    def twoSum(numbers: Array[Int], target: Int): Array[Int] = {
-        val map = scala.collection.mutable.Map[Int, Int] ()
-        var count = 0;
-        
-        for(number <- numbers){
-            val compare = target - number
-            map.get(compare) match {
-                case None => {map.put(number,count)}
-                case Some(index) => return Array(index,count) 
-            }
-            count = count+1
-        }
-        Array(0,0)
+    def check(nums: Array[Int], target : Int, current: Int, cursor: Int): Array[Int] = {
+        if (current > nums.length) Array.emptyIntArray
+        else if (current != cursor && nums(current) + nums(cursor) == target) Array(current,cursor)      
+        else if (cursor < nums.length - 1) check(nums, target, current, cursor + 1)      
+        else check(nums, target, current + 1, 1)
+    }
+    def twoSum(nums: Array[Int], target: Int): Array[Int] = {
+        check(nums, target, 0, 1)
     }
 }
