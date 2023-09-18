499716b5f5121b1b8be68691810f13e59fa91eba
kevin-zhao-career <71047577+kevin-zhao-career@users.noreply.github.com>
2023-09-18 09:51:22 -0400

    Update 0009-palindrome-number.py

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -16,11 +16,11 @@ class Solution:
         if isInvalidArgument(integer):
             return False
 
-        reversed_num = 0
+        reversed_number = 0
         original = integer
 
-        while integer > reversed_num:
-            reversed_num = reversed_num * 10 + getLastDigit(integer)
+        while integer > reversed_number:
+            reversed_number = reversed_number * 10 + getLastDigit(integer)
             integer //= 10
 
-        return integer == reversed_num or integer == reversed_num // 10
+        return (integer == reversed_number) or (integer == reversed_number // 10)
