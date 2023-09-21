00609ef4ad845aae3bbc75096a6be2671816d22c
kevin-zhao-career <71047577+kevin-zhao-career@users.noreply.github.com>
2023-09-21 15:47:21 -0400

    Update 1665-minimum-initial-energy-to-finish-tasks.py

--- 1665-minimum-initial-energy-to-finish-tasks/1665-minimum-initial-energy-to-finish-tasks.py
+++ 1665-minimum-initial-energy-to-finish-tasks/1665-minimum-initial-energy-to-finish-tasks.py
@@ -2,6 +2,11 @@
 # Author: Kevin Zhao
 # Time Complexity: O(n log n)
 # Space Complexity: O(1) if argument reuse, otherwise O(n)
+#
+# Although, there is a sort involved, the trick to this solution is to ask the following:
+# If for some k elements, the algorithm is true, what does the k+1th element do for our solution?
+# Either the minimum energy we have to expend for the k+1th element is so large that the minimum effort for our k elements does
+# not allow us to take that element, or it is enough, and we just have to expend the actual energy for the k+1th element.
 
 def getActualEnergy(task : List[int]) -> int:
     return task[0]
