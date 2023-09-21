b88676fbf48d07ab177696263cd172d9a22c0b0a
kevin-zhao-career <71047577+kevin-zhao-career@users.noreply.github.com>
2023-09-21 13:46:59 -0400

    Update 1660-correct-a-binary-tree.py

--- 1660-correct-a-binary-tree/1660-correct-a-binary-tree.py
+++ 1660-correct-a-binary-tree/1660-correct-a-binary-tree.py
@@ -5,21 +5,56 @@
 #         self.left = left
 #         self.right = right
 
-# DFS O(n) O(n)
+# Breadth First Search
+# Time Complexity: O(N)
+# Space Complexity: O(N)
+
+def setChildNodeToNone(parentNode : TreeNode, childNode : TreeNode):
+    if parentNode is None:
+        return
+    
+    if (parentNode.left is not None) and parentNode.left == childNode:
+        parentNode.left = None
+
+    if (parentNode.right is not None) and parentNode.right == childNode:
+        parentNode.right = None
+
+    return
+
+def getValues(currentLevelNodeToParentDict : dict[TreeNode, TreeNode]):
+    return [currentLevelNode.val for currentLevelNode in currentLevelNodeToParentDict.keys()]
+
+def getNextLevelNodeToParentDict(currentLevelNodeToParentDict : dict[TreeNode, TreeNode]) -> Tuple[dict[TreeNode, TreeNode], bool]:
+    foundInvalidNode = False
+    nextLevelNodeToParentDict = {}
+
+    for currentLevelNode, parentNode in currentLevelNodeToParentDict.items():
+        if currentLevelNode.left is not None:
+            nextLevelNodeToParentDict[currentLevelNode.left] = currentLevelNode
+
+        invalidCandidate = currentLevelNode.right
+        if invalidCandidate is None:
+            continue
+
+        nextLevelNodeToParentDict[invalidCandidate] = currentLevelNode
+
+        if (invalidCandidate in currentLevelNodeToParentDict):
+            foundInvalidNode = True
+            parentNode = currentLevelNodeToParentDict[currentLevelNode]            
+            setChildNodeToNone(parentNode, currentLevelNode)
+            break
+
+    return (nextLevelNodeToParentDict, foundInvalidNode)
 
 class Solution:
     def correctBinaryTree(self, root: TreeNode) -> TreeNode:
-        seen = set()
-
-        def dfs(root):
-            if not root or (root.right and root.right.val in seen):
-                return
-
-            seen.add(root.val)
-            root.right = dfs(root.right)
-            root.left = dfs(root.left)
-            
+        if root is None:
             return root
 
+        currentLevelNodeToParentDict = {root : None}
+        while currentLevelNodeToParentDict:
+            (currentLevelNodeToParentDict, foundInvalidNode) = getNextLevelNodeToParentDict(currentLevelNodeToParentDict)
+            if foundInvalidNode:
+                break
 
-        return dfs(root)
+        return root
