ab65d18ecd9e648833314ba430b31c9a2aaf5d3d
kevin-zhao-career <71047577+kevin-zhao-career@users.noreply.github.com>
2023-09-22 21:29:29 -0400

    Update 0002-add-two-numbers.py

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -10,15 +10,17 @@
 #         self.next = next
 
 DEFAULT_VALUE : int = 0
+DEFAULT_CARRY : int = 0
+BASE : int = 10
 
 def getNextNode(node : Optional[ListNode]) -> Optional[ListNode]:
     return (None if (node is None) else node.next)
 
-def getNextNodes(nodePair : Tuple[Optional[ListNode], Optional[ListNode]]) -> Tuple[Optional[ListNode], Optional[ListNode]]:
+def getNextNodePair(nodePair : Tuple[Optional[ListNode], Optional[ListNode]]) -> Tuple[Optional[ListNode], Optional[ListNode]]:
     return (getNextNode(nodePair[0]), getNextNode(nodePair[1]))
 
-def getNextNodes(nodePair : Tuple[Optional[ListNode], Optional[ListNode]], node: Optional[ListNode]) -> Tuple[Tuple[Optional[ListNode], Optional[ListNode]], optional[Node]:
-    return (getNextNodes(nodePair), node.next if (node is not None) else None)
+def getNextNodes(nodePair : Tuple[Optional[ListNode], Optional[ListNode]], node: Optional[ListNode]) -> Tuple[Tuple[Optional[ListNode], Optional[ListNode]], Optional[ListNode]]:
+    return (getNextNodePair(nodePair), node.next if (node is not None) else None)
 
 def hasNextNode(node : Optional[ListNode]) -> bool:
     return (node is not None) and (node.next is not None)
@@ -26,27 +28,36 @@ def hasNextNode(node : Optional[ListNode]) -> bool:
 def hasNextNodes(nodePair : Tuple[Optional[ListNode], Optional[ListNode]]) -> bool:
     return hasNextNode(nodePair[0]) or hasNextNode(nodePair[1])
 
-def getValue(node : ListNode) -> int:
-    return node.val
-
 def getValue(node : Optional[ListNode]) -> int:
-    return getValue(node) if node is not None else DEFAULT_VALUE
+    return node.val if (node is not None) else DEFAULT_VALUE
 
-def addNumbers(number1 : int, number2 : int, carry : int) -> int:
-    return (number1 + number2 + number3)
+def getCarry(number : int, base : int) -> int:
+    return (number / base)
 
-def addTwoNumbers(node1 : ListNode, node2 : ListNode, carry : int, reuseExistingData : bool) -> Tuple[ListNode, int]:
-    return False
+def getRemainder(number : int, base : int) -> int:
+    return (number % base)
 
-def addTwoNumbers(digitList1: Optional[ListNode], digitList2: Optional[ListNode], reuseExistingData : bool) -> Optional[ListNode]):
+def addNumbers(node1 : Optional[ListNode], node2 : Optional[ListNode], carry : int, reuseExistingData : bool) -> Tuple[Optional[ListNode], int]:
+    if ((node1 is None) and (node2 is None) and (carry == 0)):
+        return (None, DEFAULT_CARRY)
+
+    number = sum([getValue(node1), getValue(node2), carry])
+    nextCarry = getCarry(number, BASE)
+    remainder = getRemainder(number, BASE)
+    return (ListNode(remainder), nextCarry)
+
+def addTwoNumbers(digitList1: Optional[ListNode], digitList2: Optional[ListNode], reuseExistingData : bool) -> Optional[ListNode]:
     nodePair = (digitList1, digitList2)
     beginNode = endNode = None
-    carry : int = 0
+    carry : int = DEFAULT_CARRY
 
     while (hasNextNodes(nodePair)):
-        (endNode.next, carry) = addTwoNumbers(nodePair[0], nodePair[1], carry, reuseExistingData)
-        
-        if beginNode is not None:
+        if endNode is None:
+            (endNode, carry) = addNumbers(nodePair[0], nodePair[1], carry, reuseExistingData)
+        else:
+            (endNode.next, carry) = addNumbers(nodePair[0], nodePair[1], carry, reuseExistingData)
+
+        if beginNode is None:
             beginNode = endNode
 
         (nodePair, endNode) = getNextNodes(nodePair, endNode)
