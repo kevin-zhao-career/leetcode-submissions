cad2f61aa11e4c749a234320c5783afa3591ee3f
kevin-zhao-career <71047577+kevin-zhao-career@users.noreply.github.com>
2023-09-24 16:06:32 -0400

    Update 0005-longest-palindromic-substring.py

--- 0005-longest-palindromic-substring/brute_force/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/brute_force/0005-longest-palindromic-substring.py
@@ -1,1 +1,10 @@
+# 5. Longest Palindromic Substring
+# Author: Kevin Zhao
+# Time Complexity: O(n^3)
+# Space Complexity: O(1) if we return the indicies, otherwise, O(n) to store the string
 
+def checkPalindrome()
+
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        
