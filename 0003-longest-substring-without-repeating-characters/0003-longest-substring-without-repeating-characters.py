8da5d8bd89d1b71dcc8593a8421efc1fb4ab407a
kevin-zhao-career <71047577+kevin-zhao-career@users.noreply.github.com>
2023-09-24 11:38:49 -0400

    Update 0003-longest-substring-without-repeating-characters.py

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -3,6 +3,10 @@
 # Time Complexity: O(n)
 # Space Complexity: O(1) since there are at most a constant number of characters
 MAXIMUM_LIMIT = 26
+def getUpdatedCurrentSubstringBeginIndex(characterToIndexDict, character, currentSubstringBeginIndex):
+    return ((characterToIndexDict[character] + 1) if ((characterToIndexDict[character] + 1) > currentSubstringBeginIndex)
+                else (currentSubstringBeginIndex))
+
 
 class Solution:
     def lengthOfLongestSubstring(self, sentence: str) -> int:
@@ -11,10 +15,10 @@ class Solution:
 
         currentSubstringBeginIndex = 0
         for index, character in enumerate(sentence):
-            if (character in characterToIndexDict) and (characterToIndexDict[character] + 1 > currentSubstringBeginIndex):
-                currentSubstringBeginIndex = characterToIndexDict[character] + 1
+            if (character in characterToIndexDict):
+                currentSubstringBeginIndex = getUpdatedCurrentSubstringBeginIndex(characterToIndexDict, character, currentSubstringBeginIndex)
+            
             characterToIndexDict[character] = index
-
             maximumNonrepeatingSubstringLength = max(maximumNonrepeatingSubstringLength,
                 index - currentSubstringBeginIndex + 1)
 
