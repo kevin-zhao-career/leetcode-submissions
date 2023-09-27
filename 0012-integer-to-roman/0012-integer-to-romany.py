908706be7178d8edfa21edd062bde0a58b45d3b8
kevin-zhao-career <71047577+kevin-zhao-career@users.noreply.github.com>
2023-09-27 13:56:03 -0400

    Update 0012-integer-to-romany.py

--- 0012-integer-to-roman/0012-integer-to-romany.py
+++ 0012-integer-to-roman/0012-integer-to-romany.py
@@ -54,16 +54,17 @@ NUMBER_POWER_TO_LETTER_LISTS = getBaseTenOrderedDict(4)
 def getDigit(number : int, currentPower : int) -> int:
     return int((number % (currentPower * BASE)) / currentPower)
 
+def getRomanNumeralString(number : int, currentBasePower :int, romanNumeralStringList : List[str]) -> str:
+    currentPower = BASE**currentBasePower
+    letterList = NUMBER_POWER_TO_LETTER_LISTS[currentPower]
+    digit = getDigit(number, currentPower)
+    letters = letterList[digit]
+    romanNumeralStringList.append(letters)  
+    return (getRomanNumeralString(number, currentBasePower - 1, romanNumeralStringList) if (currentBasePower > 0) else
+        ("".join(romanNumeralStringList)))
+
 class Solution:
     def intToRoman(self, number: int) -> str:
         stringList = []
-        currentPower = 1000
-        while (currentPower > 0):
-            letterList = NUMBER_POWER_TO_LETTER_LISTS[currentPower]
-            digit = getDigit(number, currentPower)
-            letters = letterList[digit]
-            stringList.append(letters)
-            currentPower /= 10
-            currentPower = int(currentPower)
-        return "".join(stringList)
-        
+        currentBasePower = 3
+        return getRomanNumeralString(number, currentBasePower, stringList)
