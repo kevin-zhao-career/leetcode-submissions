b31b3d1ca3a46a4eda3c83284b13284bc94a381b
kevin-zhao-career <71047577+kevin-zhao-career@users.noreply.github.com>
2023-09-27 13:30:51 -0400

    Update 0012-integer-to-romany.py

--- 0012-integer-to-roman/0012-integer-to-romany.py
+++ 0012-integer-to-roman/0012-integer-to-romany.py
@@ -5,18 +5,37 @@
 
 from collections import OrderedDict
 
+BASE = 10
+HALFWAY_BASE = (BASE / 2)
+
+ROMAN_NUMERAL_LIST : List[str] = ["I", "V", "X", "L", "C", "D", "M"]
+
+def isEven(number : int) -> bool:
+    return ((number % 2) == 0)
+
+def isMultipleOfFive(number : int) -> bool:
+    return ((number % 5) == 0)
+
+def oneAwayMultipleOfFive(number : int) -> bool:
+    return isMultipleOfFive(number + 1)
+
+def getBaseFiveString(digit : int) -> str:
+    return ROMAN_NUMERAL_LIST[digit]
+
 def generateNumberLists(singleDigitString : str, fiveDigitString : str, tenDigitString) -> List[str]:
     numberList = []
-    for number in range(0, 10):
-        if number == 9:
-            numberList.append(singleDigitString + tenDigitString)
-        elif number == 4:
-            numberList.append(singleDigitString + fiveDigitString)
+    for number in range(0, BASE):
+        if oneAwayMultipleOfFive(number):
+            baseFiveString = tenDigitString if (number >= 9) else fiveDigitString
+            numberList.append(singleDigitString + baseFiveString)
         elif number >= 5:
             numberList.append(fiveDigitString + (singleDigitString * (number - 5)))
         else:
             numberList.append(singleDigitString * number) 
     return numberList
+
+def getBaseTenOrderedDict(maximumBasePower : int) -> OrderedDict[int, List[str]]:
+    return OrderedDict([])
 
 NUMBER_POWER_TO_LETTER_LISTS = OrderedDict([
     (1, generateNumberLists("I", "V", "X")),
@@ -26,7 +45,7 @@ NUMBER_POWER_TO_LETTER_LISTS = OrderedDict([
 ])
 
 def getDigit(number : int, currentPower : int) -> int:
-    return int((number % (currentPower * 10)) / currentPower)
+    return int((number % (currentPower * BASE)) / currentPower)
 
 class Solution:
     def intToRoman(self, number: int) -> str:
