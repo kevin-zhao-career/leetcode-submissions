fbc698147676bfb9ffa73da688e16127521a4785
kevin-zhao-career <71047577+kevin-zhao-career@users.noreply.github.com>
2023-09-29 10:59:50 -0400

    Update 0023-merge-k-sorted-lists.py

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
@@ -10,18 +10,20 @@
 #         self.next = next
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        dummy = ListNode(-1)                 # creating a garbage node whose location we will always know and the sorted list will be appended from here
-        heap = []
-
-        for n in lists:                      # run for number of linked lists times i.e. [[A], [B], [C]]
-            while n:                         # run for number of elements in a particular LL times i.e. [x, y, z]
-                heapq.heappush(heap, n.val)  # In heap using Heap-Queue property insert the value n, such that the heap remains sorted in ascending order
-                n = n.next
-
-        cur = dummy                          # we take a current pointer to point at the dummy node, so we can append our sorted LL
-        while heap:
-            temp = ListNode(heapq.heappop(heap))  # converting the heap into Node(val,nxt) one by one
-            cur.next = temp                       # dummy(cur) -> Node1[val]
-            cur = cur.next                        # dummy -> Node1[val](cur)
-        cur.next = None                           # dummy -> Node1[val] -> Node1[val] -> Node1[val] -> Node1[val](cur) -> Null
-        return dummy.next
+        head = ListNode(None)
+        curr = head
+        h = []
+        for i in range(len(lists)):
+            if lists[i]:
+                heapq.heappush(h, (lists[i].val, i))
+                lists[i] = lists[i].next
+        
+        while h:
+            val, i = heapq.heappop(h)
+            curr.next = ListNode(val)
+            curr = curr.next
+            if lists[i]:
+                heapq.heappush(h, (lists[i].val, i))
+                lists[i] = lists[i].next
+        
+        return head.next
