5dc37a83a1d3935e02671e77a68b3e8c1bc10ea2
kevin-zhao-career <71047577+kevin-zhao-career@users.noreply.github.com>
2025-03-13 11:57:41 -0400

    Update 0027-remove-element.cpp

--- 0027-remove-element/0027-remove-element.cpp
+++ 0027-remove-element/0027-remove-element.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     int removeElement(vector<int> & numbers, int value) {
-        auto last_non_value_iterator = remove(begin(numbers), end(numbers), value);
-        return distance(begin(numbers), last_non_value_iterator);
+        return distance(begin(numbers), remove(begin(numbers), end(numbers), value));
     }
 };
