119d20d631a087338355dc673a9e75b3ee043f95
kevin-zhao-career <71047577+kevin-zhao-career@users.noreply.github.com>
2025-03-13 12:14:10 -0400

    Update 26-remove-duplicates-from-sorted-array.cpp

--- 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.cpp
+++ 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.cpp
@@ -1,14 +1,12 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& numbers) {
-        unordered_set<int> number_set;
-        return distance(begin(numbers), remove_if(begin(numbers), end(numbers),
-        [&](auto && number) {
-            auto number_exists = (number_set.find(number) != end(number_set));
-            if (!number_exists) {
-                number_set.insert(number);
-            }
-            return number_exists;
-        }));
+    int count = 0;
+    int n = numbers.size();
+    for(int i = 1; i < n; i++){
+        if(numbers[i] == numbers[i-1]) count++;
+        else numbers[i-count] = numbers[i];
+    }
+        return n-count;
     }
 };
